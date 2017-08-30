2376f570700172fee9bd1495f462d08fa4e70bf4
Badangel <728532543@qq.com>
2017-08-30 16:48:38 +0800

    pick syscall from system map
    in windows

---
+++ tool/getAddrfromSysmap/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include<fstream>
+#include<string>
+using namespace std;
+
+int main()
+{
+
+    ifstream f2("syscallnum.txt");
+    ofstream f3("syscallres.txt");
+    string f1name,f1b,f2name,f1ad;
+    int f2num;
+    while(f2>>f2name){
+        f2>>f2num;
+       // printf("%s:%d\n",f2name.c_str(),f2num);
+       int a = 0;
+       ifstream f1("syscalladd.txt");
+        while(f1>>f1ad>>f1b>>f1name){
+            if(f1name == f2name){
+                f3<<"{.num = "<<f2num<<", .addr = 0x"<<f1ad.c_str()<<", .name = \""<<f2name.c_str()<<"\", .pre = 0x00},\n";
+                a = 1;
+                break;
+            }
+        }
+        if(a == 0){
+            f3<<"{.num = "<<f2num<<", .addr = 0x0000000000000000, .name = \""<<f2name.c_str()<<"\", .pre = 0x00},\n";
+        }
+    }
+    cout << "Hello world!" << endl;
+    return 0;
+}
