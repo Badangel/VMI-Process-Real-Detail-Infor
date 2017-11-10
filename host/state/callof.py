ee697c342b88938f38c0d58b9b56d67e5eaa9614
Badangel <728532543@qq.com>
2017-11-10 16:49:59 +0800

    add calculate LOF funtion

---
+++ host/state/callof.py
@@ -0,0 +1,124 @@
+import DBHelper
+import math
+import Queue
+
+
+  
+
+#Calculate the distance of each point
+def getdisance(data,datalen):
+    #print data
+    dismatrix = [[0 for i in range(datalen)] for i in range(datalen)]
+    
+    for i in range(datalen-1):
+        for j in range(i+1,datalen):
+            dis = 0
+            for l in range(len(data[0])):
+                #print("i:",i," j:",j," k:",k)
+                dis = dis + (data[i][l]-data[j][l])*(data[i][l]-data[j][l])
+            dis = math.sqrt(dis)
+            dismatrix[i][j] = dis
+            dismatrix[j][i] = dis
+    return dismatrix
+
+
+#Calculate the K distance of each point
+def getk_distance(matrix,datalen,k):
+    kdis = [0 for i in range(datalen)]
+    for i in range(datalen):
+        pq = Queue.PriorityQueue(k)
+        for j in range(datalen):
+            if i!=j:
+                if pq.full() == False:
+                    pq.put(matrix[i][j]*-1)
+                else:
+                    kdis[i] = pq.get()
+                    if matrix[i][j]*-1>kdis[i]:
+                        pq.put(matrix[i][j]*-1)
+                    else:
+                        pq.put(kdis[i])
+        kdis[i] = pq.get()*-1
+    print("kids:",kdis)
+    return kdis
+
+#Calcilate reachable disdance of each point
+def getreach_distance(matrix,datalen,kdis):
+    reachdis_matrix = [[0 for i in range(datalen)] for i in range(datalen)]
+    for i in range(datalen):
+        for j in range(datalen):
+            if i==j:
+                reachdis_matrix[i][j] = 0
+            else:
+                if matrix[i][j] > kdis[j]:
+                    reachdis_matrix[i][j] = matrix[i][j]
+                else:
+                    reachdis_matrix[i][j] = kdis[j]
+    #print("reachdis_matrix:"reachdis_matrix)
+    return reachdis_matrix
+
+
+#Calcilate local reachable density of each point
+def getlrd(reachdis_matrix,matrix,datalen,minpts):
+    lrd = [0 for i in range(datalen)]
+    for i in range(datalen):
+        lrdpq = Queue.PriorityQueue(minpts)
+        for j in range(datalen):
+            if i!=j:
+                if lrdpq.full() == False:
+                    lrdpq.put([matrix[i][j]*-1,j])
+                else:
+                    temp = lrdpq.get()
+                    if matrix[i][j]*-1 > temp[0]:
+                        lrdpq.put([matrix[i][j]*-1,j])
+                    else:
+                        lrdpq.put(temp)
+        while not lrdpq.empty():
+            temp = lrdpq.get()
+            lrd[i] = lrd[i] + reachdis_matrix[i][temp[1]]
+        lrd[i] = minpts/lrd[i]
+
+    print("lrd:",lrd)
+    return lrd
+
+                        
+#Calcilate LOF of each point
+def getlof(data,k,minpts):
+    datalen = len(data)
+    dismatrix = getdisance(data,datalen)
+    kdis = getk_distance(dismatrix,datalen,k)
+    reach_mat = getreach_distance(dismatrix,datalen,kdis)
+    lrd = getlrd(reach_mat,dismatrix,datalen,minpts)
+
+    lof = [0 for i in range(datalen)]
+    for i in range(datalen):
+        lofpq = Queue.PriorityQueue(minpts)
+        for j in range(datalen):
+            if i!=j:
+                if lofpq.full() == False:
+                    lofpq.put([dismatrix[i][j]*-1,j])
+                else:
+                    temp = lofpq.get()
+                    if dismatrix[i][j]*-1 > temp[0]:
+                        lofpq.put([dismatrix[i][j]*-1,j])
+                    else:
+                        lofpq.put(temp)
+        while not lofpq.empty():
+            temp = lofpq.get()
+            lof[i] = lof[i] + lrd[temp[1]]
+        lof[i] = (lof[i]/minpts)/lrd[i]
+
+    print"lof:",lof
+    return lof
+
+K = 4
+MinPts = 4
+db = DBHelper.DBHelper()
+sql = "select 100-idl_cpu_in,usep_mem_in,usep_swap_in,pagein_in,pageout_in,interrupts1_in,interrupts2_in,interrupts3_in,loadavg1_in*100,loadavg5_in*100,loadavg15_in*100,read_total_in,writ_total_in,ps_root_in,ps_other_in,use_cpu_out,recv_net_out,send_net_out,lsmod_out,ps_out from state"
+data = db.oncesql(sql)
+datalen = len(data) 
+print "len: ",datalen,len(data[0])
+getlof(data,K,MinPts)
+
+
+
+	
