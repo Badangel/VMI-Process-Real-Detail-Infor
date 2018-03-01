51d38abe0476fe6a926191390eda49c70133783c
Badangel <728532543@qq.com>
2018-03-01 16:55:44 +0800

    add repeat connect to dom0

--- client/state_client.py
+++ client/state_client.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/python
 ''' 
 def stat():   
     stat = {}  
@@ -14,13 +14,13 @@ def stat():
     return linecpu
 '''
 import subprocess
-#get dstat value
-stat_popen = subprocess.Popen(['dstat','-cdgilmnprsy','--noheaders'], stdout = subprocess.PIPE)
-
+import sys
+import socket 
+import time 
 import commands
 def ps_stat():
     #output = commands.getstatusoutput('ps -aux')
-    ps_res = subprocess.Popen(['ps','axo','user,pid,time,%cpu,%mem'], stdout=subprocess.PIPE)
+    ps_res = subprocess.Popen(['ps','axco','user,pid,time,%cpu,%mem,command'], stdout=subprocess.PIPE)
     return ps_res
     
 def module_stat():
@@ -28,42 +28,59 @@ def module_stat():
     lsmod_res = subprocess.Popen(['lsmod'], stdout=subprocess.PIPE)
     return lsmod_res    
  
-import socket 
-import time 
+if __name__=='__main__':
+    HOST = '223.3.85.28'  
+    PORT = 9999  
+    
+    s = socket.socket()
+    while True:
+        try:
+            #s.settimeout(1)
+            s.connect((HOST,PORT))
+            print "connect success!"
+            #get dstat value
+            stat_popen = subprocess.Popen(['dstat','-cdgilmnprsy','--noheaders'],stdout = subprocess.PIPE)
+            break
+        except:
+            print "connect fail, again"
+            time.sleep(2)
+    a=0
+    dstate = 1
+    #si=len(stat_popen.stdout.readline())
+    #print ('changdu',si)
 
-HOST = '223.3.85.28'  
-PORT = 9999  
-  
-s = socket.socket()
-while True:
-    try:
-        s.settimeout(1)
-        s.connect((HOST,PORT))
-        print "connect success!"
-        break
-    except:
-        print "connect fail, again"
-        time.sleep(2)
-a=0; 
-#si=len(stat_popen.stdout.readline())
-#print ('changdu',si)
+    while True:  
+        #kel = raw_input('>>>') 
+        a=a+1
+    # print stat() 
+        dstat='u'
+        while dstat[0] != ' ':
+            dstat=stat_popen.stdout.readline()
+        #   print dstat[0]
+    # print dstat
+        try:
+            s.sendall('s\n'+dstat)  
+            s.sendall('p\n'+ps_stat().stdout.read()+str(time.time())+' p p p p p')
+            s.sendall('m\n'+module_stat().stdout.read())      
+            #sys.stdout.flush()
+            print s.recv(1024)
+        #time.sleep(1) 
+        #if a == 10:  
+            #break 
+        except:
+            stat_popen.kill()
+            s.close()
+            s = socket.socket()
+            while True:
+                try:
+                #s.settimeout(1)
+                    s.connect((HOST,PORT))
+                    print "connect success!"
+                    stat_popen = subprocess.Popen(['dstat','-cdgilmnprsy','--noheaders'],stdout = subprocess.PIPE)
+                    #get dstat value
+                    break
+                except:
+                    print "lost connect, again"
+                    time.sleep(2)
 
-while True:  
-    #kel = raw_input('>>>') 
-    a=a+1
-   # print stat() 
-    dstat='u'
-    while dstat[0] != ' ':
-        dstat=stat_popen.stdout.readline()
-     #   print dstat[0]
-   # print dstat
-    s.sendall('s\n'+dstat)  
-    s.sendall('p\n'+ps_stat().stdout.read())
-    s.sendall('m\n'+module_stat().stdout.read())
-    print s.recv(1024) 
-    #time.sleep(1) 
-    if a == 10:  
-        break 
-
-stat_popen.kill()
-s.close()
+    s.close()
