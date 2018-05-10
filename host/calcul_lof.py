db8775148cf0b881bac8c5721bda4e0a1d090e82
Badangel <728532543@qq.com>
2018-05-10 22:35:38 +0800

    spilt sklearn funtion to calcut_sklearn

--- host/calcul_lof.py
+++ host/calcul_lof.py
@@ -14,15 +14,7 @@ import state.global_var as Globalvar
 import sys
 import signal
 from state.warning_log import *
-from sklearn import tree
 
-from sklearn import linear_model
-from sklearn import naive_bayes
-from sklearn import neighbors
-from sklearn import svm
-from sklearn import ensemble
-from sklearn.model_selection import cross_val_score
-from sklearn import preprocessing
 #Calculate the distance of each point
 
 def getdisance(data,datalen):
@@ -222,17 +214,22 @@ def detectState(domname,sqltable):
     print "statedata len:",datalen
     kdis,lrd = trainNormaiData(statedata,datalen,K,MinPts)
 
+    state_detect = open('/home/vmi/Downloads/code/VmiXen/host/tempfile/'+domname+'.statedetect', 'w')
+    state_detect.write("2")
+    state_detect.close()
+    state_Det = 2
+
 
     sqlstate = "select id,100-idl_cpu_in,usep_mem_in,usep_swap_in,pagein_in,pageout_in,interrupts1_in,interrupts2_in,interrupts3_in,loadavg1_in*100,loadavg5_in*100,loadavg15_in*100,int_sys_in*10,csw_sys_in*10,read_total_in/10,writ_total_in/10,ps_root_in,ps_other_in,use_cpu_out,recv_net_out,recv_netp_out,send_net_out,send_netp_out,lsmod_out,lsmod_out-lsmod0_in-lsmod1_in-lsmod2_in-lsmodother_in,ps_out-ps_root_in-ps_other_in from "+sqltable+" where stat = 0 and domname = '"+domname+"'"
     i = 0
-    while True:
+    while True and state_Det!=0:
         i = i + 1       
         statedatanew = selectdb.oncesql(sqlstate)
         statedatanew = list(statedatanew)
         datanewlen = len(statedatanew)
         if datanewlen == 0:
             time.sleep(1)
-            printlog("sleep 1!!"+str(i))
+            printlog("detect sleep 1!!"+str(i))
             continue
         else:
             printlog('start time:'+str(time.clock()))
@@ -240,7 +237,7 @@ def detectState(domname,sqltable):
             for a in range(0,datanewlen):
                 alof = getoplof(statedata,K,MinPts,datalen,kdis,lrd,statedatanew[a][1:])
                 changestate = ""
-                if alof > 1.5 or alof < 0.7:
+                if alof > 1.8 or alof < 0.7:
                     changestate = "update "+sqltable+" set stat = 1,lof="+str(alof)+" where id =" + str(statedatanew[a][0])
                     addnum = addnum + 1
                     selectdb.myupdate(changestate)
@@ -258,6 +255,12 @@ def detectState(domname,sqltable):
                     printlog('state move out '+str(statedatanew[a][0])+' '+str(alof)+' '+str(time.clock()))
                     #print 'state move out '+str(statedatanew[a][0])+' '+str(alof)+' '+str(time.clock())
             selectdb.allcommit()
+        state_detect = open('/home/vmi/Downloads/code/VmiXen/host/tempfile/'+domname+'.statedetect', 'r')
+        state_Det = int(state_detect.readline())
+        state_detect.close()
+        if state_Det==0:
+            printlog("stop detect state!")
+            break
 
 
 def detectPsinfo(domname,sqltable):
@@ -362,127 +365,6 @@ def trainpsinfo(domname,sqltable,maxnum):
 
         db.allcommit()
         printlog('add: '+str(addnum))
-
-def trainmode(mode):
-    
-    db = DBHelper.DBHelper()
-    sqlps = "select layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation,state from trainset"
-    psdata = db.oncesql(sqlps)
-    psdata = list(psdata)
-    X=[]
-    Y=[]
-    for l in psdata:
-        X.append(l[:-1])
-        Y.append(l[-1])
-    print len(X),len(Y),len(X[0])
-    #normalized_X = preprocessing.normalize(X)
-    scaler = preprocessing.StandardScaler().fit(X)
-    #scaled_X = preprocessing.scale(X)
-    scaled_X = scaler.transform(X)
-    score = cross_val_score(mode,scaled_X,Y)
-    print score.mean()
-    mode.fit(scaled_X,Y)
-    #print mode.feature_importances_
-    print "train over!"
-    sqlpsone = "select id,psid,psname,layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation from testset where state = 0 and domname = '"+domname+"'"
-    psonedata = db.oncesql(sqlpsone)
-    psonedata = list(psonedata)
-    datanewlen = len(psonedata)
-    for a in psonedata:
-        x_test = a[3:]
-        x_id = a[0:2]
-        x_stand = scaler.transform([x_test])
-        y_test = mode.predict(x_stand)
-        changestate = "update testset set state = "+str(y_test[0])+" where id =" + str(a[0])
-        print a[0],a[2]," is ",y_test[0]
-        db.oncesql(changestate)
-    db.allcommit()
-
-def selectmode(domname):
-    from hpsklearn import HyperoptEstimator,random_forest,svc,knn,decision_tree,ada_boost,multinomial_nb,extra_trees
-    db = DBHelper.DBHelper()
-    sqlps = "select layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation,state from trainset"
-    psdata = db.oncesql(sqlps)
-    psdata = list(psdata)
-    X=[]
-    Y=[]
-    for l in psdata:
-        X.append(l[:-1])
-        Y.append(l[-1])
-    print len(X),len(Y),len(X[0])
-    scaler = preprocessing.StandardScaler().fit(X)
-    scaled_X = scaler.transform(X)
-    mode = HyperoptEstimator(classifier=random_forest('myrandom_forest'))
-    #mode = ensemble.RandomForestClassifier()
-    mode.fit(scaled_X,Y)
-    print mode.best_model()
-    #print mode()
-    '''
-    sqlps = "select layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation,state from testset"
-    psdata1 = db.oncesql(sqlps)
-    psdata1 = list(psdata1)
-    testX=[]
-    testY=[]
-    for l in psdata1:
-        testX.append(l[:-1])
-        testY.append(l[-1])
-    testX_scale = scaler.transform(testX)
-    print mode
-    print(mode.score(testX_scale, testY))
-    '''
-    print "train over!"
-    sqlpsone = "select id,psid,psname,layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation from abnpsinfo where id>3993 and state = 0 and domname = '"+domname+"'"
-    psonedata = db.oncesql(sqlpsone)
-    psonedata = list(psonedata)
-    datanewlen = len(psonedata)
-    for a in psonedata:
-        x_test = a[3:]
-        x_id = a[0:2]
-        x_stand = scaler.transform([x_test])
-        y_test = mode.predict(x_stand)
-        changestate = "update abnpsinfo set state = "+str(y_test[0])+" where id =" + str(a[0])
-        print a[0],a[2]," is ",y_test[0]
-        db.oncesql(changestate)
-    db.allcommit()
-
-def detectpsmode(domname):
-    from hpsklearn import HyperoptEstimator,random_forest,svc,knn,decision_tree,ada_boost,multinomial_nb,extra_trees
-    db = DBHelper.DBHelper()
-    sqlps = "select layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation,state from trainset"
-    psdata = db.oncesql(sqlps)
-    psdata = list(psdata)
-    X=[]
-    Y=[]
-    for l in psdata:
-        X.append(l[:-1])
-        Y.append(l[-1])
-    print len(X),len(Y),len(X[0])
-    scaler = preprocessing.StandardScaler().fit(X)
-    scaled_X = scaler.transform(X)
-    #mode = HyperoptEstimator(classifier=random_forest('myrandom_forest'))
-    mode = ensemble.RandomForestClassifier()
-    mode.fit(scaled_X,Y)
-    #print mode.best_model()
-    #print mode()
-    Globalvar.settrainover()
-    print "train over!"
-    sqlpsone = "select id,psid,psname,layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation from testset where state = 0 and domname = '"+domname+"'"
-    while True:
-        psonedata = db.oncesql(sqlpsone)
-        psonedata = list(psonedata)
-        datanewlen = len(psonedata)
-        if datanewlen==0:
-            time.sleep(1)
-            continue
-        for a in psonedata:
-            x_test = a[3:]
-            x_id = a[0:2]
-            x_stand = scaler.transform([x_test])
-            y_test = mode.predict(x_stand)
-            changestate = "update testset set state = "+str(y_test[0])+" where id =" + str(a[0])
-            #print a[0],a[2]," is ",y_test[0]
-            db.oncesql(changestate)
-        db.allcommit()
 
 def detectAllState1(domname):
     selectdb = DBHelper.DBHelper()
@@ -615,55 +497,7 @@ if __name__ =='__main__':
                 t0 = threading.Thread(target = trainpsinfo,args =[domname,'testset'],name='testpsinfo')
                 t0.setDaemon(True)
                 t0.start()
-            if operation==9:
-                mode = tree.DecisionTreeClassifier(criterion='entropy')
-                print "tree.DecisionTreeClassifier(criterion=/'entropy/')"
-                trainmode(mode)
-            if operation==10:
-                mode = linear_model.LogisticRegression()
-                print "mode = linear_model.LogisticRegression"
-                trainmode(mode)
-            if operation==11:
-                mode = naive_bayes.GaussianNB()
-                print "mode = naive_bayes.GaussianNB"
-                trainmode(mode)
-            if operation==12:
-                mode = neighbors.KNeighborsClassifier()
-                print "mode = neighbors.KNeighborsClassifier"
-                trainmode(mode)
-            if operation==13:
-                mode = svm.SVC()
-                print "mode = svm.SVC"
-                trainmode(mode)
-            if operation==14:
-                mode = ensemble.RandomForestClassifier()
-                print "mode = ensemble.RandomForestClassifier"
-                trainmode(mode)
-            if operation==15:
-                mode = ensemble.ExtraTreesClassifier()
-                print "mode = ensemble.ExtraTreesClassifier"
-                trainmode(mode)
-            if operation==16:
-                mode = ensemble.GradientBoostingClassifier()
-                print "mode = ensemble.GradientBoostingClassifier"
-                trainmode(mode)
-            if operation==17:
-                mode = ensemble.AdaBoostClassifier()
-                print "mode = ensemble.AdaBoostClassifier"
-                trainmode(mode)
-            if operation==18:
-                print "test select mode!"
-                selectmode()
-            if operation==19:
-                print 'enter randome forest detect domU psinfo'
-                t0 = threading.Thread(target = detectpsmode,args =[domname],name='detectPsmode')
-                t0.setDaemon(True)
-                t0.start()
-                while Globalvar.gettrainover():
-                   time.sleep(1)
-                t1 = threading.Thread(target = runpsinfo,args =[domname,'2'],name='getpsinfo')
-                t1.setDaemon(True)
-                t1.start()
+            
         Globalvar.setexit()
         exdata.stopServer(Globalvar.getser())
         
---
+++ host/calcul_sklearn.py
@@ -0,0 +1,246 @@
+#!/usr/bin/python
+from state.DBHelper import DBHelper
+import math
+import Queue
+import time,threading
+
+
+import sys
+
+from state.warning_log import *
+from sklearn import tree
+
+from sklearn import linear_model
+from sklearn import naive_bayes
+from sklearn import neighbors
+from sklearn import svm
+from sklearn import ensemble
+from sklearn import neural_network
+from sklearn.model_selection import cross_val_score
+from sklearn import preprocessing
+from hpsklearn import HyperoptEstimator,random_forest,svc,knn,decision_tree,ada_boost,multinomial_nb,extra_trees,gradient_boosting,sgd
+
+def trainmode(mode):
+    
+    db = DBHelper.DBHelper()
+    sqlps = "select layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation,state from trainset"
+    psdata = db.oncesql(sqlps)
+    psdata = list(psdata)
+    X=[]
+    Y=[]
+    for l in psdata:
+        X.append(l[:-1])
+        Y.append(l[-1])
+    print len(X),len(Y),len(X[0])
+    #normalized_X = preprocessing.normalize(X)
+    scaler = preprocessing.StandardScaler().fit(X)
+    #scaled_X = preprocessing.scale(X)
+    scaled_X = scaler.transform(X)
+    score = cross_val_score(mode,scaled_X,Y)
+    print score.mean()
+    mode.fit(scaled_X,Y)
+    #print mode.feature_importances_
+    print "train over!"
+    sqlpsone = "select id,psid,psname,layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation from testset where state = 0 and domname = '"+domname+"'"
+    psonedata = db.oncesql(sqlpsone)
+    psonedata = list(psonedata)
+    datanewlen = len(psonedata)
+    for a in psonedata:
+        x_test = a[3:]
+        x_id = a[0:2]
+        x_stand = scaler.transform([x_test])
+        y_test = mode.predict(x_stand)
+        changestate = "update testset set state = "+str(y_test[0])+" where id =" + str(a[0])
+        print a[0],a[2]," is ",y_test[0]
+        db.oncesql(changestate)
+    db.allcommit()
+
+def selectmode(domname):
+    
+    db = DBHelper.DBHelper()
+    sqlps = "select layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation,state from trainset"
+    psdata = db.oncesql(sqlps)
+    psdata = list(psdata)
+    X=[]
+    Y=[]
+    for l in psdata:
+        X.append(l[:-1])
+        Y.append(l[-1])
+    print len(X),len(Y),len(X[0])
+    scaler = preprocessing.StandardScaler().fit(X)
+    scaled_X = scaler.transform(X)
+    mode = HyperoptEstimator(classifier=random_forest('myRF'))
+    #mode = HyperoptEstimator(classifier=svc('mysvc'))
+    #mode = HyperoptEstimator(classifier=extra_trees('myextra_trees'))
+    #mode = svm.SVC()
+    #mode = ensemble.ExtraTreesClassifier()
+    #mode = neighbors.KNeighborsClassifier()
+    #mode = ensemble.RandomForestClassifier()
+    #mode = tree.DecisionTreeClassifier(criterion='gini')
+    #mode = tree.DecisionTreeClassifier(criterion='entropy')
+    #mode = ensemble.AdaBoostClassifier()
+    #mode = neural_network.MLPClassifier()
+    #mode = ensemble.GradientBoostingClassifier()
+    #mode = naive_bayes.BernoulliNB()
+    #mode = naive_bayes.GaussianNB()
+    #mode = linear_model.LogisticRegression()
+    mode.fit(scaled_X,Y)
+    print mode.best_model()
+    #print mode()
+ 
+    print "train over!"
+    sqlpsone = "select id,psid,psname,layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation from abnpsinfo where state = 0 and domname = '"+domname+"'"
+    psonedata = db.oncesql(sqlpsone)
+    psonedata = list(psonedata)
+    datanewlen = len(psonedata)
+    for a in psonedata:
+        x_test = a[3:]
+        x_id = a[0:2]
+        x_stand = scaler.transform([x_test])
+        y_test = mode.predict(x_stand)
+        changestate = "update abnpsinfo set state = "+str(y_test[0])+" where id =" + str(a[0])
+        print a[0],a[2]," is ",y_test[0]
+        db.oncesql(changestate)
+    db.allcommit()
+
+def detectpsmode(domname):
+    from hpsklearn import HyperoptEstimator,random_forest,svc,knn,decision_tree,ada_boost,multinomial_nb,extra_trees
+    db = DBHelper.DBHelper()
+    sqlps = "select layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation,state from trainset"
+    psdata = db.oncesql(sqlps)
+    psdata = list(psdata)
+    X=[]
+    Y=[]
+    for l in psdata:
+        X.append(l[:-1])
+        Y.append(l[-1])
+    print len(X),len(Y),len(X[0])
+    scaler = preprocessing.StandardScaler().fit(X)
+    scaled_X = scaler.transform(X)
+    #mode = HyperoptEstimator(classifier=random_forest('myrandom_forest'))
+    mode = ensemble.RandomForestClassifier()
+    mode.fit(scaled_X,Y)
+    #print mode.best_model()
+    #print mode()
+    Globalvar.settrainover()
+    print "train over!"
+    sqlpsone = "select id,psid,psname,layer,prio,inc_minflt,inc_majflt,inc_utime,inc_stime,mm_users,mm_count,stack_vm,unix, netlink,tcp,udp,tcpv6,eventfd,inotify, timerfd, signalfd, eventpoll, pipe, filenum,ps_control,file_rw,file_control,sys_control,mem_control,net_control,socket_control,user_control,ps_communcation from testset where state = 0 and domname = '"+domname+"'"
+    while True:
+        psonedata = db.oncesql(sqlpsone)
+        psonedata = list(psonedata)
+        datanewlen = len(psonedata)
+        if datanewlen==0:
+            time.sleep(1)
+            continue
+        for a in psonedata:
+            x_test = a[3:]
+            x_id = a[0:2]
+            x_stand = scaler.transform([x_test])
+            y_test = mode.predict(x_stand)
+            changestate = "update testset set state = "+str(y_test[0])+" where id =" + str(a[0])
+            #print a[0],a[2]," is ",y_test[0]
+            db.oncesql(changestate)
+        db.allcommit()
+
+def clearwarning():
+    file_module = open('log/warning.log', 'r+')
+    file_module.truncate()
+    file_module.close()
+
+def clearprintlog():
+    file_module = open('log/print.log', 'r+')
+    file_module.truncate()
+    file_module.close()
+def clearfilelog():
+    file_module = open('log/file.log', 'r+')
+    file_module.truncate()
+    file_module.close()
+
+def quit(signum,frame):
+    Globalvar.setexit()
+    exdata.stopServer(Globalvar.getser())
+    print "you stop calcul_lof!!"
+    sys.exit()
+
+if __name__ =='__main__':
+    try:
+        signal.signal(signal.SIGINT, quit)
+        signal.signal(signal.SIGTERM, quit)
+        
+        clearwarning()
+        clearprintlog()
+        clearfilelog()
+        while over:
+            print "enter your command:"
+            command = raw_input()
+            comands = command.split()
+            if len(comands)==2:
+                domname,operation = command.split()
+            else:
+                if command == 'q':
+                    break
+                else:
+                    print "command error !"
+                    continue
+            operation = int(operation)
+            print "(command:)",command
+
+            if operation==9:
+                mode = tree.DecisionTreeClassifier(criterion='entropy')
+                print "tree.DecisionTreeClassifier(criterion=/'entropy/')"
+                trainmode(mode)
+            if operation==10:
+                mode = linear_model.LogisticRegression()
+                print "mode = linear_model.LogisticRegression"
+                trainmode(mode)
+            if operation==11:
+                mode = naive_bayes.GaussianNB()
+                print "mode = naive_bayes.GaussianNB"
+                trainmode(mode)
+            if operation==12:
+                mode = neighbors.KNeighborsClassifier()
+                print "mode = neighbors.KNeighborsClassifier"
+                trainmode(mode)
+            if operation==13:
+                mode = svm.SVC()
+                print "mode = svm.SVC"
+                trainmode(mode)
+            if operation==14:
+                mode = ensemble.RandomForestClassifier()
+                print "mode = ensemble.RandomForestClassifier"
+                trainmode(mode)
+            if operation==15:
+                mode = ensemble.ExtraTreesClassifier()
+                print "mode = ensemble.ExtraTreesClassifier"
+                trainmode(mode)
+            if operation==16:
+                mode = ensemble.GradientBoostingClassifier()
+                print "mode = ensemble.GradientBoostingClassifier"
+                trainmode(mode)
+            if operation==17:
+                mode = ensemble.AdaBoostClassifier()
+                print "mode = ensemble.AdaBoostClassifier"
+                trainmode(mode)
+            if operation==18:
+                print "test select mode!"
+                selectmode(domname)
+            if operation==19:
+                print 'enter randome forest detect domU psinfo'
+                t0 = threading.Thread(target = detectpsmode,args =[domname],name='detectPsmode')
+                t0.setDaemon(True)
+                t0.start()
+                while Globalvar.gettrainover():
+                   time.sleep(1)
+                t1 = threading.Thread(target = runpsinfo,args =[domname,'2'],name='getpsinfo')
+                t1.setDaemon(True)
+                t1.start()
+        Globalvar.setexit()
+        exdata.stopServer(Globalvar.getser())
+        
+        print "exit calcul_lof normal!!"
+
+    except Exception,exc:
+        print "calcul_lof over",exc
+    
+
+    
